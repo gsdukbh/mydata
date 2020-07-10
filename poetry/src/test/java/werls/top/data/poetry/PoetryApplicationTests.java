ea4b47aae160baf8f753a296c760c0d2caf2b14f
lijiawei <gsdukbh@gmail.com>
2020-07-10 14:18:31 +0800

    构建项目

---
+++ poetry/src/test/java/werls/top/data/poetry/PoetryApplicationTests.java
@@ -0,0 +1,13 @@
+package werls.top.data.poetry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoetryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
