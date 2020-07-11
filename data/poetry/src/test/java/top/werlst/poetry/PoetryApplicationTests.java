ab015866204b067aac771f74492e1daf5569984f
gsdukbh <gsdukbh@gmail.com>
2020-07-11 20:37:00 +0800

    初次配置

---
+++ data/poetry/src/test/java/top/werlst/poetry/PoetryApplicationTests.java
@@ -0,0 +1,13 @@
+package top.werlst.poetry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoetryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ data/src/main/java/top/werls/data/DataApplication.java
@@ -0,0 +1,18 @@
+package top.werls.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author Lee JiaWei
+ *  项目
+ */
+@SpringBootApplication
+public class DataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataApplication.class, args);
+    }
+
+}
